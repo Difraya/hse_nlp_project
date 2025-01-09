7f7b2902c2707531b82a075e71ac7bc705532aee
Yulia_Smiyan <143211897+Difraya@users.noreply.github.com>
2025-01-09 14:39:29 +0300

    убрала опечатку(лишние кавычки)

--- streamlit/nltk_download.py
+++ streamlit/nltk_download.py
@@ -1,7 +1,7 @@
 import nltk
 
 def download_nltk_resources():
-    resources = [''punkt_tab'', 'stopwords', 'averaged_perceptron_tagger_eng']
+    resources = ['punkt_tab', 'stopwords', 'averaged_perceptron_tagger_eng']
     for resource in resources:
         try:
             nltk.data.find(f"tokenizers/{resource}")
